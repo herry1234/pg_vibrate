d7806651d82f51c6e976913df453f4d38b420efe
rick <richardhurst@live.com>
2013-06-25 16:20:25 -0700

    tap abstraction with touch slide

--- scripts/main.js
+++ scripts/main.js
@@ -65,9 +65,49 @@ App.LetsGo = (function(){
 (function init(){
 	$("#vibrate_length_input").change(App.Vibrate.setVibrateDuration);
 	$("[data-page='vibrate'] form").submit(App.Vibrate.doVibrate);
-	$("#toggle").click(function(){
-		$(this).toggleClass("clicked");
-	});
+	
+	//move this
+	var toggle_state_boo = 0;
+	if("ontouchmove" in $("<div />")[0]){
+		$(document).on("touchmove", "#toggle", function(tme){
+			var offset_left_num = tme.target.offsetLeft,
+				finger_left_num = tme.originalEvent.clientX,
+				button_width = $(this).width();
+				half_way_point = Math.ceil(button_width/2);
+			if((finger_left_num - offset_left_num) > half_way_point){
+				toggle_state_boo = 1;
+				console.log("finger " + ((finger_left_num - offset_left_num) - half_way_point) + " px to the right of the half_way_point, trigger ON if not on");
+			}else{
+				toggle_state_boo = 0;
+				console.log("finger " + (half_way_point - (finger_left_num - offset_left_num)) + " px to the left of the half_way_point, trigger OFF if not oFF");
+			}
+		});
+		$(document).on("touchend", "#toggle", function(tme){
+			console.log("Touchend fired: Toggle state is now: " + toggle_state_boo);
+			if(toggle_state_boo){
+				$(this).removeClass("clicked").addClass("clicked");
+			}else{
+				$(this).removeClass("clicked");
+			}
+		});
+	}else{
+		$("#toggle").click(function(){
+			if(toggle_state_boo){
+				toggle_state_boo = 0;
+				$(this).removeClass("clicked");
+			}else{
+				toggle_state_boo = 1;
+				$(this).removeClass("clicked").addClass("clicked");
+			}
+		});
+	}
+
+
+
+	// $("#toggle").swipe(function(se){
+	// 	console.dir(se);
+	// 	$(this).toggleClass("clicked");
+	// });
 })();
 
 if(APP_PRODUCTION_TYPE === 1 || APP_PRODUCTION_TYPE === 3){
